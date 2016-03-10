1d9bcdce455fa8fc744866c967da5e6aeee99b2c
Tim Dang <timothy.dang@gmail.com>
2016-03-10 15:24:28 -0800

    Unit tests section complete.

--- test/unit/utilities.js
+++ test/unit/utilities.js
@@ -3,12 +3,30 @@ var should = chai.should();
 var expect = chai.expect;
 
 describe('the todo.App', function() {
-  describe('the todo object', function(){
+  describe('the todo object', function() {
+    expect(todo).to.be.a('object');
 
-    it('should have all the necessary methods', function(){
+    it('should have all the necessary methods', function() {
+      assert.property(todo.util, 'trimTodoName');
+      todo.util.should.have.property('isValidTodoName');
+      expect(todo.util).to.have.property('getUniqueId');
     });
   });
 });
 
 describe('the todo.util methods', function() {
+  describe('the trimTodoName function', function() {
+    todo.util.trimTodoName(' hello').should.equal('hello');
+    todo.util.trimTodoName(' hello').should.not.equal(' hello');
+    todo.util.trimTodoName(' hello').should.have.length(5);
+  });
+
+  describe('the isValidTodoName function', function() {
+    expect(todo.util.isValidTodoName('#')).to.equal(false);
+    expect(todo.util.isValidTodoName('SamSmith')).to.equal(true);
+  });
+
+  describe('the getUniqueId function', function() {
+    assert.typeOf(todo.util.getUniqueId(), 'number');
+  });
 });
