2793faf646858e7478090a143997e64519d132c7
Tim Dang <timothy.dang@gmail.com>
2016-03-10 18:09:39 -0800

    Completed integration testing.

--- test/integration/apiTests.js
+++ test/integration/apiTests.js
@@ -1,6 +1,33 @@
-describe('API integration', function(){
+describe('API integration', function() {
   var server, setupStub, JSONresponse;
 
-  it('todo.setup receives an array of todos when todo.init is called', function () {
+  before(function() {
+    JSONresponse = {
+      todos: [{
+        name: "First item",
+        done: true
+      }, {
+        name: "Second item",
+        done: false
+      }]
+    };
+    server = sinon.fakeServer.create();
+    setupStub = sinon.stub(todo, 'setup');
+    server.respondWith("GET", "http://localhost:3000/todos", [200, {
+        "Content-Type": "application/json"
+      },
+      JSON.stringify(JSONresponse)
+    ]);
+  });
+  after(function() {
+    server.restore();
+    todo.setup.restore();
+  });
+
+  it('todo.setup receives an array of todos when todo.init is called', function() {
+    todo.init();
+    server.respond();
+    assert(setupStub.calledOnce);
+    assert(setupStub.calledWith(JSONresponse.todos));
   });
 });
